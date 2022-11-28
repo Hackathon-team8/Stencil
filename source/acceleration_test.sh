9f0a87e041dbd019485c7da55fe49d3e57fa9fb3
Ennaox <alexis.laplanche@hotmail.fr>
2022-11-28 16:17:58 +0100

    Modification des scripts: choix du nombre d'itération

--- source/acceleration_test.sh
+++ source/acceleration_test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-./base_stencil 100 100 100 5 > result_base.dat
+./base_stencil 100 100 100 $1 > result_base.dat
 
 
 ./compil.sh
-./stencil 100 100 100 5 > result_accelerated.dat
+./stencil 100 100 100 $1 > result_accelerated.dat
 
-python3 test_result.py
+python3 test_result.py $1
