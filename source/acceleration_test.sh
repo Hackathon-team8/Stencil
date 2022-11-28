ba662df20b9dfc64ce236a7a28b49335040a4a5d
Ennaox <alexis.laplanche@hotmail.fr>
2022-11-28 10:51:56 +0000

    Modification du scripr: changement des nom des .dat

--- source/acceleration_test.sh
+++ source/acceleration_test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-./base_stencil 100 100 100 5 > result_stencil.dat
+./base_stencil 100 100 100 5 > result_base.dat
 
 
 ./compil.sh
-./stencil 100 100 100 5 > result_compil.dat
+./stencil 100 100 100 5 > result_accelerated.dat
 
 python3 test_result.py
